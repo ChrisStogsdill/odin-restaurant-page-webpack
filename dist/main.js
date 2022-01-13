6931cad8ba3c9bb912b46939682f67cdcc6a406c
ChrisStogsdill <chris1stogsdill@gmail.com>
2022-01-13 16:15:52 -0600

    Start with Image

--- dist/main.js
+++ dist/main.js
@@ -23,21 +23,55 @@ eval("const content = document.getElementById('content');\r\n\r\nconsole.log('te
 /*!*************************!*\
   !*** ./src/mainPage.js ***!
   \*************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("console.log('main page js testing')\n\n//# sourceURL=webpack://odin-restaurant-page-webpack/./src/mainPage.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ createMainPage)\n/* harmony export */ });\n \r\nfunction createMainPage() {\r\n    const content = document.getElementById('content');\r\n    const picture = new Image();\r\n        picture.src = './transister.png'\r\n\r\n    const title = content.appendChild(document.createElement('h1'));\r\n    title.innerText = 'Title of restaurant'\r\n\r\n}\r\n\r\ncreateMainPage();\r\n\n\n//# sourceURL=webpack://odin-restaurant-page-webpack/./src/mainPage.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	__webpack_modules__["./src/index.js"](0, {}, __webpack_require__);
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/mainPage.js"]();
+/******/ 	__webpack_modules__["./src/mainPage.js"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
--- src/mainPage.js
+++ src/mainPage.js
@@ -1,3 +1,12 @@
+ 
 export default function createMainPage() {
-const content = document.getElementById('content');
+    const content = document.getElementById('content');
+    const picture = new Image();
+        picture.src = './transister.png'
+
+    const title = content.appendChild(document.createElement('h1'));
+    title.innerText = 'Title of restaurant'
+
 }
+
+createMainPage();
