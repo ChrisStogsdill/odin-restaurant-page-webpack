1ccc8ef40d52638422af1d336c94e37bec1096b4
ChrisStogsdill <chris1stogsdill@gmail.com>
2022-01-12 21:48:55 -0600

    start filling out js

---
+++ main.js
@@ -0,0 +1,43 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const content = document.getElementById('content');\r\n\r\nconsole.log('test');\n\n//# sourceURL=webpack://odin-restaurant-page-webpack/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/mainPage.js":
+/*!*************************!*\
+  !*** ./src/mainPage.js ***!
+  \*************************/
+/***/ (() => {
+
+eval("console.log('main page js testing')\n\n//# sourceURL=webpack://odin-restaurant-page-webpack/./src/mainPage.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/mainPage.js"]();
+/******/ 	
+/******/ })()
+;
